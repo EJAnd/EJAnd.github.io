3858ebad94cbc9d7d1e7130dc4e8d54971a79a04
EJAnd <ethanjandrews96@gmail.com>
2021-06-08 17:07:36 -0400

    first commit

---
+++ JS/skillbar.js
@@ -0,0 +1,77 @@
+const progress = document.querySelector(".progress1-done");
+
+setTimeout(() => {
+  progress.style.width = progress.getAttribute('data-done') + '%';
+  progress.style.opacity = 1;
+}, 500)
+
+const proglayer = document.querySelector(".progress1-done2");
+
+setTimeout(() => {
+  proglayer.style.width = proglayer.getAttribute('data-done') + '%';
+  proglayer.style.opacity = 1;
+}, 500)
+
+// --------------------------------------------------------------- PROGBAR ONE
+
+const progress2 = document.querySelector(".progress2-done");
+
+setTimeout(() => {
+  progress2.style.width = progress2.getAttribute('data-done') + '%';
+  progress2.style.opacity = 1;
+}, 500)
+
+const proglayer2 = document.querySelector(".progress2-done2");
+
+setTimeout(() => {
+  proglayer2.style.width = proglayer2.getAttribute('data-done') + '%';
+  proglayer2.style.opacity = 1;
+}, 500)
+
+// --------------------------------------------------------------- PROGBAR TWO
+
+const progress3 = document.querySelector(".progress3-done");
+
+setTimeout(() => {
+  progress3.style.width = progress3.getAttribute('data-done') + '%';
+  progress3.style.opacity = 1;
+}, 500)
+
+const proglayer3 = document.querySelector(".progress3-done2");
+
+setTimeout(() => {
+  proglayer3.style.width = proglayer3.getAttribute('data-done') + '%';
+  proglayer3.style.opacity = 1;
+}, 500)
+
+// --------------------------------------------------------------- PROGBAR THREE
+
+const progress4 = document.querySelector(".progress4-done");
+
+setTimeout(() => {
+  progress4.style.width = progress4.getAttribute('data-done') + '%';
+  progress4.style.opacity = 1;
+}, 500)
+
+const proglayer4 = document.querySelector(".progress4-done2");
+
+setTimeout(() => {
+  proglayer4.style.width = proglayer4.getAttribute('data-done') + '%';
+  proglayer4.style.opacity = 1;
+}, 500)
+
+// --------------------------------------------------------------- PROGBAR THREE
+
+const progress5 = document.querySelector(".progress5-done");
+
+setTimeout(() => {
+  progress5.style.width = progress5.getAttribute('data-done') + '%';
+  progress5.style.opacity = 1;
+}, 500)
+
+const proglayer5 = document.querySelector(".progress5-done2");
+
+setTimeout(() => {
+  proglayer5.style.width = proglayer5.getAttribute('data-done') + '%';
+  proglayer5.style.opacity = 1;
+}, 500)
