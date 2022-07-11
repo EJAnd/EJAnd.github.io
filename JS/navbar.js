2bce24c971c1dfd011845d0390fbabbdb9258cf4
EJAnd <84740887+EJAnd@users.noreply.github.com>
2022-07-10 22:32:07 -0400

    Add files via upload

---
+++ JS/navbar.js
@@ -0,0 +1,19 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
+
+    burger.addEventListener('click', () => {
+        //toggle navigation
+        nav.classList.toggle('nav-active');
+        //Animate link entry
+        navLinks.forEach((link, index) => {
+            link.style.animation = `navLinkFade 0.25s ease forwards ${index/8}s`
+            console.log(index / 8);
+        });
+    });
+    
+    
+}
+
+navSlide();
